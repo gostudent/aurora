5009070aa868283dbef209f9779d3d16738f8e66
Vishal Pani <panivishal17@gmail.com>
2018-01-17 23:19:42 +0530

    Gradients added

--- examples/example_1.go
+++ examples/example_1.go
@@ -8,5 +8,6 @@ func main() {
 	quote := []string{"Be yourself", "everyone else is", "already taken."}
 	author := "Oscar Wilde"
 	filename := "1"
+	//Optional argument for gradient color added indexed from 1
 	aurora.CreatePort(filename, quote, author)
 }
